a4274909e755fe4361b556ff4c708b9407ee09a7
XEric21 <ericcasanovas3@gmail.com>
2026-01-30 17:39:03 +0100

    Añadidas excepciones en la función de calcular las notas

--- funciones1/notas_y_asistencia.py
+++ funciones1/notas_y_asistencia.py
@@ -4,84 +4,60 @@ def notas_y_asistencia():
 
 
     while True:
-        n1 = input("Tu nota de la primera evaluación: ").replace(",", ".")
-
-        if n1.replace(".", "", 1).isdigit(): #Cambiamos
-            n1 = float(n1)
+        try:
+            n1 = input("Tu nota de la primera evaluación: ").replace(",", ".")
+            n1 = float(n1)               
             if 0 <= n1 <= 10:
                 print(Fore.GREEN + "La nota es válida ✅" + Style.RESET_ALL)
                 break
             else:
                 print(Fore.RED + "Esa nota no es válida ❌" + Style.RESET_ALL)
-        else:
-            print(Fore.RED + "Solo puedes poner números ❌" + Style.RESET_ALL)
+        except ValueError:
+            print ("Solo se pueden poner números")
 
 
 
     while True:
-        n2 = input("Tu nota de la segunda evaluación: ").replace(",", ".")
-    
-        if n2.replace(".", "", 1).isdigit():
+        try:
+            n2 = input("Tu nota de la segunda evaluación: ").replace(",", ".")
             n2 = float(n2)
             if 0 <= n2 <= 10:
                 print(Fore.GREEN + "La nota es válida ✅" + Style.RESET_ALL)
                 break
             else:
                 print(Fore.RED + "Esa nota no es válida ❌" + Style.RESET_ALL)
-        else:
-                print(Fore.RED + "Solo puedes poner números ❌" + Style.RESET_ALL)
+        except ValueError:
+            print ("Solo se pueden poner números")
 
     while True:
-        n3 = input("Tu nota de la tercera evaluación: ").replace(",", ".")
-    
-        if n3.replace(".", "", 1).isdigit():
+        try:
+            n3 = input("Tu nota de la tercera evaluación: ").replace(",", ".")
             n3 = float(n3)
             if 0 <= n3 <= 10:
-                print(Fore.GREEN + "La nota es válida ✅" + Style.RESET_ALL)
-                break
+                    print(Fore.GREEN + "La nota es válida ✅" + Style.RESET_ALL)
+                    break
             else:
-                print(Fore.RED + "Esa nota no es válida ❌" + Style.RESET_ALL)
-        else:
-            print(Fore.RED + "Solo puedes poner números ❌" + Style.RESET_ALL)
+                    print(Fore.RED + "Esa nota no es válida ❌" + Style.RESET_ALL)
+        except ValueError:
+            print ("Solo puedes poner números")
+       
 
 
 
     while True:
-        asistencia = input("Pon tu asistencia: ").replace(",", ".")
-    
-        if asistencia.replace(".", "", 1).isdigit():
+        try:
+            asistencia = input("Pon tu asistencia: ").replace(",", ".")
             asistencia = float(asistencia)
             if 0 <= asistencia <= 100:
-                print(Fore.GREEN + "La asistencia es válida ✅" + Style.RESET_ALL)
-                break
+                    print(Fore.GREEN + "La asistencia es válida ✅" + Style.RESET_ALL)
+                    break
             else:
-                print(Fore.RED + "Esa asistencia no es válida ❌" + Style.RESET_ALL)
-        else:
-            print(Fore.RED + "Solo puedes poner números ❌" + Style.RESET_ALL)
+                    print(Fore.RED + "Esa asistencia no es válida ❌" + Style.RESET_ALL)
+        except ValueError: 
+             print ("Solo puedes poner números")
     
     return n1, n2, n3, asistencia
 
-
-    media = (n1 + n2 + n3) /3
-    media_redondeada = round(media, 2)
-
-    if media_redondeada < 5:
-        print(Fore.RED+"Suspendido ❌"+Style.RESET_ALL)
-    else:
-        print(Fore.GREEN+"Nota aprobada ✅"+Style.RESET_ALL)
-
-    if asistencia < 85:
-        print(Fore.RED+"No llegas a la asistencia mínima ❌"+Style.RESET_ALL)
-    else:
-        print(Fore.GREEN+"Cumples con la asistencia ✅ "+Style.RESET_ALL)
-
-    promociona = False 
-    if media_redondeada >=5  and asistencia>= 85:
-        print(Fore.GREEN+"Aprueba ✅"+Style.RESET_ALL)
-        promociona = True
-    else:
-        print(Fore.RED+"Suspende ❌"+Style.RESET_ALL)
-        promociona = False
 
     
    
